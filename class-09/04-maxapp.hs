07e8fc89346784690989974072d95a30e367e023
Hequs <alexpetr-mail@yandex.ru>
2014-11-10 22:23:34 +0300

    Задание 9. Полное.

--- class-09/04-maxapp.hs
+++ class-09/04-maxapp.hs
@@ -8,24 +8,28 @@ import Control.Applicative
 -}
 
 maxApp2 :: (Ord a, Applicative f) => f a -> f a -> f a
-maxApp2 = undefined
+maxApp2 a1 a2 = max <$> a1 <*> a2
 
 {- Реализуйте аналогичную функцию в случае трёх заданных значений в контексте. -}
 
 maxApp3 :: (Ord a, Applicative f) => f a -> f a -> f a -> f a
-maxApp3 = undefined
+maxApp3 a1 a2 a3 = maxApp2 a1 (maxApp2 a2 a3)
 
 {- Реализуйте аналогичную функцию в случае списка значений в контексте. -}
 
 maxApp :: (Ord a, Applicative f) => [f a] -> f a
-maxApp = undefined
+maxApp = foldl1 maxApp2
 
 {-
   Продемонстрируйте использование написанных функций для аппликативных функторов Maybe,
   список (для каждого из двух экземпляров), Either String и IO.
 -}
 
-main = undefined
+main = do
+	print $ maxApp2 (Just 2) (Just 3)
+	print $ maxApp3 (Just 1) (Just 2) (Just 3)
+	print $ maxApp [Nothing, Just 1, Just 2, Just 3]
+	print $ maxApp [Left "Left" :: Either String Int, Right 1 :: Either String Int]
 
 {- (необязательно)
   Ясно ли вам, что вы реализовали нечто, похожее на моноид на аппликативных функторах?
--- class-10/05-intersect.hs
+++
@@ -1,3 +0,0 @@
-{- Пользуясь списком как монадой, вычислите пересечение  заданных списков -}
-intersect :: Eq a => [[a]] -> [a]
-intersect = foldr undefined undefined
