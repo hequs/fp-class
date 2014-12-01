9508f0fc4d8cfd074de7fb1dbdb0fd7a2f1fccbc
Hequs <alexpetr-mail@yandex.ru>
2014-12-01 23:16:26 +0300

    Задание 1.

--- class-12/01-password.hs
+++ class-12/01-password.hs
@@ -8,28 +8,61 @@
    - все попытки ввода пароля протоколируются средствами монады Writer.
 -}
 
+import System.Environment
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
-
+import Control.Monad.Writer
+import Control.Monad.Reader
+import Control.Monad.Writer
 import Data.Char
 
-isValid :: String -> Bool
-isValid s = length s >= 8 && 
-                any isAlpha s && 
-                any isNumber s && 
-                any isPunctuation s
+{- Config -}
+type Config = (Int, [Bool])
 
-getValidPassword :: MaybeT IO String
+parseConfig :: [String] -> Config
+parseConfig (len : params) = (read len :: Int, map (\x -> read x :: Bool) params)
+
+
+{- isValid -}
+isValid :: String -> Reader Config Bool
+isValid s = do
+	(len, [a, n, p]) <- ask
+	return $ length s >= len 
+                && if a then any isAlpha s else True
+                && if n then any isNumber s else True
+                && if p then any isPunctuation s else True
+
+
+{- getValidPassword -}
+getValidPassword :: MaybeT (ReaderT Config (WriterT [String] IO)) String
 getValidPassword = do
-  lift $ putStrLn "Введите новый пароль:"
-  s <- lift getLine
-  guard (isValid s)
+  liftIO $ putStrLn "Введите новый пароль:"
+  s <- liftIO getLine
+  tell [s]
+  config <- lift ask
+  guard (runReader (isValid s) config)
   return s
- 
-askPassword :: MaybeT IO ()
-askPassword = do
-  value <- msum $ repeat getValidPassword
-  lift $ putStrLn "Сохранение в базе данных..."
 
-main = runMaybeT askPassword
+
+{- askPassword -}
+askPassword :: MaybeT (ReaderT Config (WriterT [String] IO)) ()
+askPassword = do	
+	value <- msum $ repeat getValidPassword
+	liftIO $ putStrLn "Сохранение в базе данных..."
+
+	
+{- main -}	
+main = do
+	config <- getArgs
+	result <- runWriterT (runReaderT (runMaybeT askPassword) (parseConfig config))
+	return $ result
+	
+{-
+	:main "4" "False" "True" "True"
+	
+	минимальная длина 			- 4
+	наличие букв				- False
+    наличие цифр				- True
+	наличие знаков пунктуации 	- True
+-}
